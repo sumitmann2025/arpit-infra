b665118146ad68741bdd58298a9953b1c6f55384
sumitmann2025 <sumitmann69d@gmail.com>
2026-05-27 14:56:59 +0530

    Add multiple Azure resource groups in rg.tf

--- rg.tf
+++ rg.tf
@@ -6,3 +6,11 @@ resource " azurerm_resource_group" "rgs" {
 name = "rg_sumit"
 location = "eastus"
 }
+resource " azurerm_resource_group" "rgs" {
+name = "rg_arpit1"
+location = "eastus"
+}
+resource " azurerm_resource_group" "rgs" {
+name = "rg_sumit1"
+location = "eastus"
+}
